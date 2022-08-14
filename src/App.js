6dc553bf90dc9e38b1b0e7b3adbf4c6bfc6da6d6
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-08-14 02:16:19 +0200

    added a function to hanldeAddTodo and used a useRef hook to get the value off the input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React, { useState} from 'react';
+import React, { useState, useRef} from 'react';
 import TodoList from './TodoList';
 
 function App() {
-    const[todos, setTodos] = useState([{id: 1, name: 'Todo 1', complete: false}])
+    const[todos, setTodos] = useState([])
+    const todoNameRef = useRef()
+
+    function handleAddTodo(e){
+        const name = todoNameRef.current.value
+
+        // if input is empty string it will not give back anything
+        if (name === '') return;
+
+        setTodos(previousTodos => {
+            return [...previousTodos,{id: 1, name: name, complete: false}]
+        })
+
+        // input field will get emptied after input has been rendered
+        todoNameRef.current.value = null
+    }
     return (
         <>
             <TodoList todos={todos} setTodos={setTodos}/>
-            <input type="text"/>
-            <button>Add todo</button>
+            <input ref= {todoNameRef} type="text"/>
+            <button onClick={handleAddTodo}>Add todo</button>
             <button>clear completed todos</button>
             <div>0 left to do</div>
         </>
